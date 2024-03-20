edfa839ddf9daef905306a644f8353179004e3b9
quithro <143888319+quithro@users.noreply.github.com>
2024-03-19 20:26:41 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const mainRouter = require("./routes/index");
 
 const app = express();
-
 const { PORT = 3001 } = process.env;
 
 mongoose
@@ -13,8 +12,15 @@ mongoose
   })
     .catch(console.error);
 
-  app.use("/", mainRouter);
   app.use(express.json());
+  app.use((req, res, next) => {
+    req.user = {
+      _id: '5d8b8592978f8bd833ca8133'
+    };
+    next();
+  });
+
+  app.use("/", mainRouter);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);

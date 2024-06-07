0845e7af3a2dfd1eecbd7e194658a66d91d91c7d
quithro <143888319+quithro@users.noreply.github.com>
2024-06-06 20:45:41 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,52 @@
+require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
 const mongoose = require("mongoose");
+
+const cors = require("cors");
+
+const app = express();
+
+const { errors } = require("celebrate");
+
+const { PORT = 3001 } = process.env;
+
+const errorHandler = require("./middlewares/error-handling");
+
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+
+const {
+  validateUserLogin,
+  validateUserBody,
+} = require("./middlewares/validation");
+
 const { login, createUser } = require("./controllers/users");
+
+app.listen(PORT, () => {
+  console.log("this is working");
+});
+
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", (r) => {
+  console.log("connected to DB", r);
+});
+app.use(cors());
 
 const routes = require("./routes/index");
 
-const app = express();
-const { PORT = 3001 } = process.env;
-
-app.listen(PORT, () => {
-  console.log(`Server started at port: ${PORT}`);
-});
-
-mongoose
-  .connect('mongodb://127.0.0.1:27017/wtwr_db', (r) => {
-    console.log("Connected to DB", r);
-});
-
-app.use(cors());
-
 app.use(express.json());
-app.post("/signin", login);
-app.post("/signup", createUser);
+app.use(requestLogger);
+
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
+app.post("/signin", validateUserLogin, login);
+app.post("/signup", validateUserBody, createUser);
 
 app.use(routes);
+
+app.use(errorLogger);
+
+app.use(errors());
+app.use(errorHandler);

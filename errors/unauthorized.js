e190797899cb7beff2641959ea245ba3f8e7e90b
quithro <143888319+quithro@users.noreply.github.com>
2024-06-13 20:21:04 -0500

    Update unauthorized.js

--- errors/unauthorized.js
+++ errors/unauthorized.js
@@ -2,6 +2,9 @@ class UnauthorizedError extends Error {
   constructor(message) {
     super(message);
     this.statusCode = 401;
+    errorlogger.error(
+      'Not authorized to access. Try again with correct credientials. (401 error)',
+    );
   }
 }
 

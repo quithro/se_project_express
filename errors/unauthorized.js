82d70a45fb090199b413069f3127a06f5a92e3b4
quithro <143888319+quithro@users.noreply.github.com>
2024-06-13 20:26:48 -0500

    Add files via upload

--- errors/unauthorized.js
+++ errors/unauthorized.js
@@ -2,9 +2,6 @@ class UnauthorizedError extends Error {
   constructor(message) {
     super(message);
     this.statusCode = 401;
-    errorlogger.error(
-      'Not authorized to access. Try again with correct credientials. (401 error)',
-    );
   }
 }
 

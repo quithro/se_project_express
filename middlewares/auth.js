62f45dba805f14a54113540fdab303b789e718d3
quithro <143888319+quithro@users.noreply.github.com>
2024-06-13 20:10:02 -0500

    Add files via upload

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,31 +1,29 @@
 const jwt = require("jsonwebtoken");
-const { UnauthorizedError } = require("../errors/unauthorized");
+const UnauthorizedError = require("../errors/unauthorized");
 
+const { NODE_ENV, JWT_SECRET } = process.env;
 
-const auth = (req, res, next) => {
+module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    next(new UnauthorizedError("Unauthorized"));
+    throw new UnauthorizedError("Unauthorized");
   }
 
   const token = authorization.replace("Bearer ", "");
 
   let payload;
-
   try {
     payload = jwt.verify(
       token,
-     process.env.NODE_ENV === "production" ? process.env.JWT_SECRET
-     : "some-secret-key",
+      process.env.NODE_ENV === 'production'
+        ? process.env.JWT_SECRET
+        : 'some-secret-key',
     );
+
   } catch (err) {
-    next(new UnauthorizedError("Unauthorized"));
+    throw new UnauthorizedError("Unauthorized");
   }
   req.user = payload;
-  next();
-};
-
-module.exports = {
-  auth,
+  return next();
 };
